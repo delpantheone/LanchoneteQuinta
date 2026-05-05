e7cbaf1903af67c20eb7fcf6e89834994d7a1793
delpantheone <gabrielpaulosilva@gmail.com>
2026-05-05 16:14:11 -0300

    tests: implement 'test_deve_cancelar_pedido_com_sucesso' in test_api_cancelar_pedido

---
+++ tests/test_api_cancelar_pedido.py
@@ -0,0 +1,24 @@
+def test_deve_cancelar_pedido_com_sucesso(client):
+    r1 = client.post("/clientes", json={"cpf": "11122233344", "nome": "Cliente X"})
+    assert r1.status_code == 200
+    r2 = client.post("/produtos", json={"codigo": "1", "valor": 15, "tipo": 1, "desconto_percentual": 10})
+    assert r2.status_code == 200
+    r3 = client.post("/produtos", json={"codigo": "2", "valor": 10, "tipo": 1, "desconto_percentual": 5})
+    assert r2.status_code == 200
+    cliente = r1.json()
+    p1 = r2.json()
+    r4 = client.post("/lanchonete/pedidos", json={"cpf": cliente["cpf"], "cod_produto": p1["codigo"], "qtd_max_produtos": 10})
+    assert r4.status_code == 200
+    pedido = r4.json()
+    p2 = r3.json()
+    r5 = client.put(f"/lanchonete/pedidos/{pedido["codigo"]}/itens", json={"cod_produto": p2["codigo"]})
+    assert r5.status_code == 200
+    
+    response = client.post("/lanchonete/pedidos/1/cancelar")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["ok"]
+    assert data["mensagem"] == "Pedido cancelado com sucesso"

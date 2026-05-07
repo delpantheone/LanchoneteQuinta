bd759e843a47ce84bfe1ed80ec65be23541aee9b
delpantheone <gabrielpaulosilva@gmail.com>
2026-05-07 20:27:43 -0300

    tests: implement 'test_deve_buscar_observacao_pedido' in test_api_observacao_pedido

--- tests/test_api_observacao_pedido.py
+++ tests/test_api_observacao_pedido.py
@@ -90,3 +90,41 @@ def test_nao_deve_adicionar_observacao_em_pedido_finalizado(client):
     )
 
     assert response.status_code == 400
+
+def test_deve_buscar_observacao_pedido(client):
+    r1 = client.post("/clientes", json={"cpf": "11122233344", "nome": "Cliente X"})
+    assert r1.status_code == 200
+    r2 = client.post(
+        "/produtos",
+        json={"codigo": "1", "valor": 15, "tipo": 1, "desconto_percentual": 10},
+    )
+    assert r2.status_code == 200
+    cliente = r1.json()
+    p1 = r2.json()
+    r3 = client.post(
+        "/lanchonete/pedidos",
+        json={
+            "cpf": cliente["cpf"],
+            "cod_produto": p1["codigo"],
+            "qtd_max_produtos": 10,
+        },
+    )
+    assert r3.status_code == 200
+    pedido = r3.json()
+
+    r4 = client.post(
+        f"/lanchonete/pedidos/{pedido['codigo']}/observacao",
+        json={"observacao": "Sem cebola"},
+    )
+
+    assert r4.status_code == 200
+
+    response = client.get(
+        "/lanchonete/pedidos/1/observacao"
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data['observacao'] == 'Sem cebola'

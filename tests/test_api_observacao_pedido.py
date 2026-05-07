a8a62f8535fba6a32770e2a9f07105ad4008f354
delpantheone <gabrielpaulosilva@gmail.com>
2026-05-07 19:41:33 -0300

    tests: implement 'test_deve_adicionar_observacao' in test_api_observacao_pedido

---
+++ tests/test_api_observacao_pedido.py
@@ -0,0 +1,29 @@
+def test_deve_adicionar_observacao(client):
+    r1 = client.post("/clientes", json={"cpf": "11122233344", "nome": "Cliente X"})
+    assert r1.status_code == 200
+    r2 = client.post(
+        "/produtos",
+        json={"codigo": "1", "valor": 15, "tipo": 1, "desconto_percentual": 10},
+    )
+    assert r2.status_code == 200
+    cliente = r1.json()
+    p1 = r2.json()
+    r3 = client.post(
+        "/lanchonete/pedidos",
+        json={
+            "cpf": cliente["cpf"],
+            "cod_produto": p1["codigo"],
+            "qtd_max_produtos": 10,
+        },
+    )
+    assert r3.status_code == 200
+    pedido = r3.json()
+    response = client.post(
+        f"/lanchonete/pedidos/{pedido['codigo']}/observacao",
+        json={"observacao": "Sem cebola"},
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+    assert data["mensagem"] == "Observação adicionada com sucesso"

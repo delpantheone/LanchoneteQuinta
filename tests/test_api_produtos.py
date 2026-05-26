ec0892864a0ed4ebace5c7f788d311b3c77ea28d
delpantheone <gabrielpaulosilva@gmail.com>
2026-05-25 22:30:21 -0300

    fix(tests): update 'test_put_produto_retorna_json_alterou_true' to use async syntax

--- tests/test_api_produtos.py
+++ tests/test_api_produtos.py
@@ -1,8 +1,12 @@
-def test_put_produto_retorna_json_alterou_true(client):
-    response = client.post("/produtos", json={"codigo": "1", "valor": 15, "tipo": 1, "desconto_percentual": 10})
+async def test_put_produto_retorna_json_alterou_true(client):
+    response = await client.post(
+        "/produtos",
+        json={"codigo": "1", "valor": 15, "tipo": 1, "desconto_percentual": 10},
+    )
     assert response.status_code == 200
     codigo = response.json()["codigo"]
-    
-    response2 = client.put(f"/produtos/{codigo}/valor", json={"novo_valor": 10})
+
+    response2 = await client.put(f"/produtos/{codigo}/valor", json={"novo_valor": 10})
     # print(response2.json())
     assert response2.json()["alterou"] == True
+
